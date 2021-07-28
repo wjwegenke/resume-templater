4238d241a7c5650325585db9898026b3a014cace
wjwegenke <wjwegg@gmail.com>
2021-07-28 02:48:21 -0500

    Added nav bar.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,50 @@
 import React, { useState, useEffect } from 'react';
 import './App.css';
+import Navigation from './components/nav/Navigation';
 import Template from './components/template/Template';
 
-function App() {
+function App(props) {
     const [resume, setResume] = useState(null);
+    const [template, setTemplate] = useState('primary');
 
     useEffect(() => {
         fetchResume();
     }, []);
 
     const fetchResume = async () => {
-        const fetchedResume = await fetch('/resume.json');
+        const fetchedResume = await fetch('/resume-templater/resume.json');
         setResume(await fetchedResume.json());
     };
 
+    const templates = [
+        {
+            value: 'primary',
+            name: 'Primary'
+        },
+        {
+            value: 'secondary',
+            name: 'Secondary'
+        }
+    ];
+
+    let templateJSX = <div className="error">No template selected</div>;
+    switch (template) {
+        case 'primary':
+            templateJSX = <Template resume={resume}/>;
+            break;
+        case 'secondary':
+            templateJSX = <div className="error">No template found</div>;
+            break;
+        default:
+            templateJSX = <Template resume={resume}/>;
+            break;
+    }
+
     return (
     <div className="App">
+        <Navigation setResume={setResume} templates={templates} template={template} setTemplate={setTemplate}/>
         <div className="resume">
-            <Template resume={resume}/>
+            {templateJSX}
         </div>
     </div>
     );
---
+++ src/components/nav/Navigation.js
@@ -0,0 +1,53 @@
+import React from 'react';
+import './Navigation.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faPrint } from '@fortawesome/free-solid-svg-icons';
+
+function Navigation(props) {
+
+    const templateOptions = props.templates?.map((t, i) => {
+        return (<option key={`template${i}`} value={t.value}>{t.name}</option>)
+    });
+
+    const updateTemplate = (e) => {
+        console.log(e.target.value);
+        props.setTemplate(e.target.value);
+    }
+
+    const updateFile = (e) => {
+        let file = e.target.files[0];
+        console.log(file);
+        
+        if (file) {
+            const fileReader = new FileReader();
+            fileReader.readAsText(file, "UTF-8");
+            fileReader.onload = e => {
+              props.setResume(JSON.parse(e.target.result));
+            };
+        }
+    };
+
+    return (
+        <nav className="navigation">
+            <h1>Resume Templator</h1>
+            <div className="inputs">
+                <div className="input-group">
+                    <label for="templateSelector">Select Tempate</label>
+                    <select id="templateSelector" onChange={updateTemplate}>
+                        {templateOptions}
+                    </select>
+                </div>
+                <div className="input-group">
+                    <label for="templateFileInput">Upload Resume JSON</label>
+                    <input type="file" name="templateFileInput" onChange={updateFile} />
+                </div>
+            </div>
+            <a href="/resume-templater/resume.json" target="_blank" rel="noreferrer">Example Resume JSON</a>
+            <div className="btn-icon" onClick={window.print}>
+                <FontAwesomeIcon icon={faPrint} />
+            </div>
+        </nav>
+    )
+}
+
+export default Navigation;
